fd21c9515af465b3410e624a9a3e564987568c44
Alex <oleksii.oliinyk.work@gmail.com>
2023-06-10 19:25:51 +0300

    implemented search bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-
-
 import './styles/App.css';
 import NabBar from './NavBar';
 import Events from './Events';
