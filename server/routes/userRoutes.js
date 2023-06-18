9a4b3934008c674f2de2611c1c40b5e2f0417c57
Alex <oleksii.oliinyk.work@gmail.com>
2023-06-18 15:57:41 +0300

    stable version 1.0

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../schemas/UserSchema');
+const {JWT_SECRET}= require('../config');
+
+// User registration
+router.post("/register", async (req, res) => {
+    const {firstName, lastName, email, password} = req.body;
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const oldUser = await User.findOne({email});
+        if (oldUser){
+        return res.send({error:"User Exists"})
+        }
+
+        await User.create({
+        firstName,
+        lastName,
+        email,
+        password: encryptedPassword,
+        });
+        res.send({status:"ok"})
+    } catch (error) {
+        res.send({status:"error"})
+    }
+});
+
+// User login
+router.post("/login-user", async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+
+    if (!user){
+        return res.send({error:"User not found"})
+    }
+    if (await bcrypt.compare(password, user.password)){
+        const token = jwt.sign({email:user.email}, JWT_SECRET);
+        if (res.status(201)){
+        return res.json({status:"ok", data:token});
+        }else{
+        return res.json({error: "error"})
+        }
+    }
+    res.json({status:"error", error:"Incorrect Password"})
+});
+
+// Get user data
+router.post("/userData", async (req, res) => {
+    const{token} = req.body;
+
+  try {
+
+    const user = jwt.verify(token, JWT_SECRET);
+
+    const useremail = user.email;
+    
+    User.findOne({email:useremail }).then((data)=>{
+      res.send({status: "ok", data: data})
+    }).catch((error) => {
+      res.send({status: "error", data: data})
+    });
+    
+  } catch (error) {
+    
+  }
+});
+
+module.exports = router;
